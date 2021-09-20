f4e04681dee85e1d4ff03de96e6205edda99e4a5
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-09-20 12:05:44 +0900

    Add files via upload

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -19,10 +19,11 @@ int win(char board[][3],int rowSize) {
         }
         i++;
     }
-    //세로검사 - j가 끝도 없이 늘어남(두번째 반복문에서)
+    //세로검사 
+   j = 0;
    while(j<3) {
         cnt = 0;
-        for (i = 0; i < 2; j++) {
+        for (i = 0; i < 2; i++) {
             if (board[i][j] == 'X' || board[i][j] == 'O') {
                 if (board[i][j] == board[i + 1][j])
                     cnt++;
@@ -34,12 +35,12 @@ int win(char board[][3],int rowSize) {
         j++;
     }
     //대각선 검사
-    if (board[0][0] != ' ' || board[1][1] == ' ' || board[2][2] != ' ') {
+    if (board[0][0] != ' ' || board[1][1] != ' ' || board[2][2] != ' ') {
         if (board[0][0] == board[1][1])
             if (board[0][0] == board[2][2])
                 return 1;
     }
-    if (board[0][2] != ' ' || board[1][1] == ' ' || board[2][0] != ' ') {
+    if (board[0][2] != ' ' || board[1][1] != ' ' || board[2][0] != ' ') {
         if (board[0][2] == board[1][1])
             if (board[0][2] == board[2][0])
                 return 1;
@@ -52,42 +53,40 @@ int main(void)
 {
     char board[3][3];
     int x, y, k, i, j;
+    int temp;
 
     for (x = 0; x < 3; x++)
         for (y = 0; y < 3; y++) board[x][y] = ' ';
 
     for (k = 0; k < 9; k++) {
         //board[x][y] = (k % 2 == 0) ? 'X' : 'O'; //돌아가면서 나오도록
-                                               //컴퓨터가 O - 나머지가 1일 때
+        temp = 0;                                      //컴퓨터가 O - 나머지가 1일 때
         if (k % 2 == 0) {
             printf("(x,y) 좌표(x,y<3):");
             scanf("%d%d", &x, &y);
 
             //(1)상대방이 놓은 곳에 다시 놓을 수 없도록 하는 코드
             if (board[x][y] == 'X' || board[x][y] == 'O') {
-                printf("이미 놓은 자리입니다!\n");
+                printf("이미 놓은 자리입니다!\n차례가 넘어갑니다\n");
                 continue;
             }  
             else 
                 board[x][y] = (k % 2 == 0) ? 'X' : 'O';
             
         }
-        //(3)컴퓨터가 자동으로 다음 수 결정하는 알고리즘-비어있는 첫번째 좌표에 넣기
-        //여기가 문제!
-        if (k % 2 == 1) {
-            for(i=0;i<3;i++)
-                for (j = 0; j < 3; j++) {
-                    if (board[i][j] == ' ') {
-                        board[x][y] = (k % 2 == 0) ? 'X' : 'O';
-                        break;
-                    }
-                }
-        }
 
-        if (k > 0) {
-            if (win(board, 3)) {
-                printf("게임이 끝났습니다");
-                break;
+        //(3)컴퓨터가 자동으로 다음 수 결정하는 알고리즘-비어있는 첫번째 좌표에 넣기
+        if (k % 2 == 1) {
+            printf("컴퓨터 차례(O)\n");
+            for(i=0;i<3;i++){
+                for (j = 0; j < 3; j++) {
+                    if (board[i][j] != 'X' && board[i][j] != 'O') {
+                        board[i][j] = (k % 2 == 0) ? 'X' : 'O';
+                        temp = 1;
+                    }
+                    if (temp == 1)
+                        i = 4;
+                }
             }
         }
 
@@ -96,6 +95,13 @@ int main(void)
             printf(" %c l %c l %c \n", board[i][0], board[i][1], board[i][2]);
         }
         printf("---l---l---\n");
+
+        if (k > 0) {
+            if (win(board, 3)) {
+                printf("게임이 끝났습니다");
+                break;
+            }
+        }
 
     }
     if (k == 9) {
