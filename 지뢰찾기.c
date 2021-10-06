cd3f897d192a3fb96e9ea4776a1218dedd59da9c
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2021-10-06 11:23:17 +0900

    Add files via upload

--- 지뢰찾기.c
+++ 지뢰찾기.c
@@ -1,4 +1,6 @@
-
+/*****************
+*지뢰찾기
+*********************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define X_VALUE 5 //2차원 배열의 행의 수
@@ -8,7 +10,7 @@ void readBombInfo(char grid[][Y_VALUE + 1])
 {
 	int i;
 	// grid 및 지뢰 정보 입력
-	printf("Input Grid\n");
+	//printf("Input Grid\n");
 	for (i = 0; i < X_VALUE; i++)
 		scanf("%s", grid[i]); // 문자열 형식으로 읽음!
 }
@@ -18,14 +20,14 @@ void countBomb(char grid[][Y_VALUE + 1], int numOfBombs[][Y_VALUE])
 	for (i = 0; i < X_VALUE; i++)
 		for (j = 0; j < Y_VALUE; j++)
 			if (grid[i][j] == '*') {
-				//여기에 지뢰의 개수를 세어 numOfBombs에 넣는 코드 작성
-				//나를 감싸고 있는 자리에 지뢰의 수 증가
 				for (int y = i - 1; y <= i + 1; y++) {
+					if (y < 0 || y >= X_VALUE)
+						continue;
 					for (int x = j - 1; x <= j + 1; x++) {
-						if (i - 1 < 0 || j - 1 < 0 || i + 1 > Y_VALUE || j + 1 > Y_VALUE)
+						if (x < 0 || x >= Y_VALUE)
 							continue;
-						else
-							numOfBombs[y][x] += 1;
+						
+						numOfBombs[y][x] += 1; //else 지움
 					}
 				}
 			}
@@ -47,7 +49,7 @@ int main(void)
 	char grid[X_VALUE][Y_VALUE + 1]; //문자열의 경우 마지막에 NULL이 들어가야 하므로
 									// 5X5 배열이 아닌 5X6 배열이 되어야 한다.
 	int numOfBombs[X_VALUE][Y_VALUE] = { 0 }; //지뢰의 개수를 넣는 정수형 5X5 배열
-	
+
 	readBombInfo(grid);
 
 	countBomb(grid, numOfBombs);
