202a72f9efb2b2dea31a14068a4d689c5ec45e8d
Yoojin Lee <80568037+yoojinlee-hub@users.noreply.github.com>
2022-02-01 18:55:14 +0900

    Add files via upload
    https://www.acmicpc.net/problem/10828

--- 스택.c
+++ 스택.c
@@ -2,27 +2,49 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-	int size[100];
-	char in[50];
-	int n = 0, i = 0;
 
-	while (1) {
+int main(void) {
+	int size[10000];
+	char in[50];
+	int n = 0, i = 0, count = 0;
+
+	scanf("%d", &count);
+
+	while (count) {
 		scanf("%s", in);
+
 
 		if (strcmp(in, "push") == 0) {
 			scanf("%d", &n);
 			size[i++] = n;
 		}
+
 		else if (strcmp(in, "pop") == 0) {
-			printf("%d", size[i - 1]);
-			size[i - 1] = NULL;
-			i--;
+			if (i > 0) {
+				printf("%d\n", size[i - 1]);
+				size[i - 1] = NULL;
+				i--;
+			}
+			else
+				printf("-1\n");
 		}
-		else if (strcmp(in, "top") == 0)
-			printf("%d", size[i-1]);
-		else if (strcmp(in, "bottom") == 0)
-			printf("%d", size[0]);
+
+		else if (strcmp(in, "size") == 0)
+			printf("%d\n", i);
+
+		else if (strcmp(in, "top") == 0) 
+			if (i > 0)
+				printf("%d\n", size[i - 1]);
+			else
+				printf("-1\n");
+			
+		else if (strcmp(in, "empty") == 0)
+			if (i > 0) 
+				printf("0\n");
+			else 
+				printf("1\n");
+		
+		count--;
 	}
-	
+	return 0;
 }
