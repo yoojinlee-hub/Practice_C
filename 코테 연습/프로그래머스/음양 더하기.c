5fd0dac09963e86653bcf6df632f76387d58ed46
YoojinLee <80568037+yoojinlee-hub@users.noreply.github.com>
2023-12-13 15:29:12 +0900

    Add files via upload

---
+++ 코테 연습/프로그래머스/음양 더하기.c
@@ -0,0 +1,22 @@
+/*
+어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+https://school.programmers.co.kr/learn/courses/30/lessons/76501
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// absolutes_len은 배열 absolutes의 길이입니다.
+// signs_len은 배열 signs의 길이입니다.
+int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
+    int answer = 0;
+    
+    for(int i=0; i<absolutes_len; i++){
+        if(signs[i]==true)
+            answer += absolutes[i];   
+        else
+            answer -= absolutes[i];    
+    }
+    
+    return answer;
+}
