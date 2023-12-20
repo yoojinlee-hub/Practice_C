8a3aa7f692cd5de00fd98e6b648171db4e92cb58
YoojinLee <80568037+yoojinlee-hub@users.noreply.github.com>
2023-12-21 08:43:17 +0900

    Add files via upload

---
+++ 코테 연습/프로그래머스/프로그래머스-가장가까운글자.c
@@ -0,0 +1,44 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/142086
+문자열 s가 주어졌을 때, s의 각 위치마다 자신보다 앞에 나왔으면서, 자신과 가장 가까운 곳에 있는 같은 글자가 어디 있는지 알고 싶습니다.
+예를 들어, s="banana"라고 할 때,  각 글자들을 왼쪽부터 오른쪽으로 읽어 나가면서 다음과 같이 진행할 수 있습니다.
+
+b는 처음 나왔기 때문에 자신의 앞에 같은 글자가 없습니다. 이는 -1로 표현합니다.
+a는 처음 나왔기 때문에 자신의 앞에 같은 글자가 없습니다. 이는 -1로 표현합니다.
+n은 처음 나왔기 때문에 자신의 앞에 같은 글자가 없습니다. 이는 -1로 표현합니다.
+a는 자신보다 두 칸 앞에 a가 있습니다. 이는 2로 표현합니다.
+n도 자신보다 두 칸 앞에 n이 있습니다. 이는 2로 표현합니다.
+a는 자신보다 두 칸, 네 칸 앞에 a가 있습니다. 이 중 가까운 것은 두 칸 앞이고, 이는 2로 표현합니다.
+따라서 최종 결과물은 [-1, -1, -1, 2, 2, 2]가 됩니다.
+
+문자열 s이 주어질 때, 위와 같이 정의된 연산을 수행하는 함수 solution을 완성해주세요.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* solution(const char* s) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(strlen(s)*sizeof(int));
+    
+    for(int i=0;i<strlen(s);i++){
+        if(i==0){
+            answer[i] = -1;
+        }else{
+            int temp = 0;
+            for(int j=i-1;j>=0;j--){
+                if(s[i]==s[j]){
+                    answer[i]=i-j;
+                    temp = 1;
+                    break;
+                }
+            }
+            if(!temp) answer[i] = -1;
+        }
+    }
+    
+    return answer;
+}
