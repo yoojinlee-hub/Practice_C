d8a61d8df406b39fcf0cadeee719f286c6f4a9d8
YoojinLee <80568037+yoojinlee-hub@users.noreply.github.com>
2023-12-22 13:21:01 +0900

    Add files via upload

---
+++ 코테 연습/프로그래머스/문자열기본.c
@@ -0,0 +1,27 @@
+/*
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+
+제한 사항
+s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+s는 영문 알파벳 대소문자 또는 0부터 9까지 숫자로 이루어져 있습니다.
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+bool solution(const char* s) {
+    int len = strlen(s);
+
+    if(4==len||6==len){
+        for (int i = 0; i < len; i++) {
+            if (s[i] >= '0' && s[i] <= '9');
+            else 
+                return false;
+        }
+    }else
+        return false;
+
+    return true;
+}
